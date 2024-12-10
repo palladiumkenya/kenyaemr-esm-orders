6cf4921a9f4994c8ca27f8c91196574c3d819710
Felix Kiprotich <72152368+FelixKiprotich350@users.noreply.github.com>
2024-12-09 19:48:23 -0800

    (Fix) Updated the procedures and imaging apps to load the current day orders by default on page load (#10)
    * added date filters
    
    * imaging orders updated
    
    * Update packages/esm-imaging-orders-app/src/shared/ui/common/grouped-orders-table.component.tsx
    
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>
    
    * Update packages/esm-procedure-orders-app/src/shared/ui/common/grouped-orders-table.component.tsx
    
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>
    
    * Update packages/esm-procedure-orders-app/src/shared/ui/common/grouped-orders-table.scss
    
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>
    
    * Update packages/esm-procedure-orders-app/src/shared/ui/common/grouped-orders-table.scss
    
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>
    
    * renamed ordertypeuuid
    
    ---------
    
    Co-authored-by: Felix Kiprotich <fkiprotich845>
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>

---
+++ packages/esm-imaging-orders-app/src/shared/ui/common/orders-date-range-picker.tsx
@@ -0,0 +1,36 @@
+import { DatePicker, DatePickerInput } from '@carbon/react';
+import { useAppContext } from '@openmrs/esm-framework';
+import dayjs from 'dayjs';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { DateFilterContext } from '../../../types';
+import styles from './orders-date-range-picker.scss';
+
+export const OrdersDateRangePicker = () => {
+  const currentDate = new Date();
+  const { dateRange, setDateRange } = useAppContext<DateFilterContext>('imaging-date-filter') ?? {
+    dateRange: [dayjs().startOf('day').toDate(), new Date()],
+    setDateRange: () => {},
+  };
+
+  const { t } = useTranslation();
+
+  const handleOrdersDateRangeChange = (dates: Array<Date>) => {
+    setDateRange(dates);
+  };
+
+  return (
+    <div className={styles.datePickerWrapper}>
+      <p>{t('dateRange', 'Date range')}:</p>
+      <DatePicker
+        datePickerType="range"
+        className={styles.dateRangePicker}
+        onClose={handleOrdersDateRangeChange}
+        maxDate={currentDate.toISOString()}
+        value={dateRange}>
+        <DatePickerInput id="date-picker-input-id-start" placeholder="dd/mm/yyyy" size="md" />
+        <DatePickerInput id="date-picker-input-id-finish" placeholder="dd/mm/yyyy" size="md" />
+      </DatePicker>
+    </div>
+  );
+};
