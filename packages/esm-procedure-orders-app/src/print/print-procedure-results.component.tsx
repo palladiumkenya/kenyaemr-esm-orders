b5e27cc840fb37398094a60bc6e642f3a8f316b4
its-kios09 <legacyitsolution@gmail.com>
2025-01-08 01:37:30 +0300

    (feat) updated the functionality of the procedure order

---
+++ packages/esm-procedure-orders-app/src/print/print-procedure-results.component.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonSet, ModalBody, ModalFooter, InlineNotification } from '@carbon/react';
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
+import styles from './print-procedure.scss';
+import { Order, Result } from '../types';
+import PrintableReport from './print-procedure.component';
+
+type PrintPreviewModalProps = {
+  onClose: () => void;
+  completedOrder: Result;
+};
+
+const PrintPreviewModal: React.FC<PrintPreviewModalProps> = ({ onClose, completedOrder }) => {
+  const { t } = useTranslation();
+  const [printError, setPrintError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const handlePrint = useReactToPrint({
+    content: () => ref.current,
+  });
+
+  return (
+    <>
+      <ModalBody>
+        <div ref={ref}>
+          <PrintableReport completedOrder={completedOrder} />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <ButtonSet className={styles.btnSet}>
+          <Button kind="secondary" onClick={onClose} type="button">
+            {t('cancel', 'Cancel')}
+          </Button>
+          <Button kind="primary" type="button" disabled={isLoading} onClick={handlePrint}>
+            {t('print', 'Print')}
+          </Button>
+        </ButtonSet>
+      </ModalFooter>
+
+      {printError && (
+        <InlineNotification kind="error" title={t('printError', 'Error')} subtitle={printError} hideCloseButton />
+      )}
+    </>
+  );
+};
+
+export default PrintPreviewModal;
